9d1b601e53b7cc3c95d439df0e1e7b3030ea8d43
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-08-24 15:41:31 -0400

    delete components to test how things look, add more components

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,14 +1,13 @@
 import React from "react";
+import "./style.css";
 
-function Jumbotron({ children }) {
-  return (
-    <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron"
-    >
-      {children}
-    </div>
-  );
+function Jumbotron() {
+    return (
+        <div className="jumbotron text-center">
+            <h1>(React) Google Books Search</h1>
+            <h3>Search for and Save Books of Interest</h3>
+        </div>
+    );
 }
 
 export default Jumbotron;

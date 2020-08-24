fd45f789bebab35d3d06f651e8241f78eb824d9c
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-08-24 19:43:00 -0400

    add routes requirement to serverjs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
 const mongoose = require("mongoose");
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +16,9 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// Define API routes here
+app.use(routes)
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });

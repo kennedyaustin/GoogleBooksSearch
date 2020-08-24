0bec852c06c00c154a450b8f17469251e48f61c3
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-08-24 18:12:35 -0400

    gave input field placeholder, centered text and button beneath the header

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,19 +1,18 @@
 import React from "react";
 
-
 function Form(props){
 return (
   <div className="container">
   <form>
-    <div className="form-group">
-      <label htmlFor="search"><h2>Search for and save Books of Interest</h2></label>
+    <div className="text-center form-group">
+      <label htmlFor="search"><h2>Search for Books on Google!</h2></label>
       <input
         onChange={props.handleInputChange}
         value={props.search}
         name="search"
         type="text"
-        className="form-control"
-        placeholder="Search a Book"
+        className="form-control inputField"
+        placeholder="Search for a Book"
         id="search"
       />
       <button onClick={props.handleFormSubmit} className="btn btn-dark mt-3 mb-5">
